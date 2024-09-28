03e29b9be2b53de4bbc5a0feb58e93849b342c56
Ziximu <97015776+Ziximu-Li@users.noreply.github.com>
2024-09-28 18:59:52 +0800

    Update GPU_calculate.py
    整理代码，提升速度

--- GPU_calculate.py
+++ GPU_calculate.py
@@ -9,10 +9,13 @@ import random
 
 seed = 111111
 E = 2.1 * 1e9  # 杨氏模量
-beta = 3 * 1e6
+beta = 1 * 1e7
 mu = 0.3  # 泊松比
 
-# 设置随机种子
+# 悬臂梁尺寸
+length = 20.0
+height = 5.0
+
 def set_seed(seed):
     random.seed(seed)
     np.random.seed(seed)
@@ -22,22 +25,16 @@ def set_seed(seed):
         torch.cuda.manual_seed_all(seed)
         torch.backends.cudnn.deterministic = True
         torch.backends.cudnn.benchmark = False
-
 set_seed(seed)
-
-# 检查是否有可用的GPU
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda")
 
 class Swish(nn.Module):
     def forward(self, x):
         return x * torch.sigmoid(x)
-
-# 定义Softplus激活函数
 class Softplus(nn.Module):
     def forward(self, x):
         return torch.nn.functional.softplus(x)
 
-# 定义神经网络结构
 class PINN(nn.Module):
     def __init__(self):
         super(PINN, self).__init__()
@@ -78,37 +75,11 @@ class PINN(nn.Module):
                 if m.bias is not None:
                     nn.init.constant_(m.bias, 0)  # 初始化偏置为0
 
-
-# 如何将输出数据反归一？
 def normalize_data(data):
     data_normalized = data.clone()  # 先克隆数据，避免在原始数据上进行in-place操作
     data_normalized[:, 0] = data_normalized[:, 0] - 10
     data_normalized[:, 1] = data_normalized[:, 1] - 2.5
     return data_normalized
-
-
-def generate_BC_training_data(length, height, step):
-    # 固定端边界条件 (x = 0)    length = 20, height = 5, step = 200
-
-    x_boundary_fixed = torch.zeros((step, 2), device=device)  # 边界坐标点
-    x_boundary_fixed[:, 1] = torch.linspace(0, height, step, device=device)  # 生成 x=0 边界的坐标  --> [0, y]T
-    x_boundary_fixed.requires_grad_(True)
-    
-    y_boundary_fixed = torch.zeros((step, 2), device=device)  # 设置固定端的位移: y_boundary_fixed --> U and V (x = 0) = 0
-
-    # 其他边界条件
-    x_boundary = torch.cat([
-        torch.cat([torch.linspace(0, length, step, device=device)[1:step-1, None], torch.zeros((step-2, 1), device=device)], dim=1),  # 下边界
-        torch.cat([torch.linspace(0, length, step, device=device)[1:step-1, None], height * torch.ones((step-2, 1), device=device)], dim=1),  # 上边界
-        torch.cat([length * torch.ones((step, 1), device=device), torch.linspace(0, height, step, device=device)[:, None]], dim=1)  # 右边界
-    ]) # 定义上下右侧边界：x_boundary --> [fy(y=0) + fy(y=5) + fx(x=20)] --> [200*3, 2]T
-    x_boundary.requires_grad_(True)
-
-    y_boundary = torch.zeros((step, 2), device=device)  # 应力载荷条件的大小
-    y_boundary[:, 0] = torch.linspace(5, 0, step, device=device)[:step]
-    y_boundary.requires_grad_(True)
-
-    return x_boundary_fixed, y_boundary_fixed, x_boundary, y_boundary
 
 def calculate_sigma(u_pred, v_pred, x_interior):
     # 几何方程
@@ -143,21 +114,31 @@ def calculate_f(sigma_xx, sigma_yy, sigma_xy, x_interior):
     return fxx_x, fxy_x, fxy_y, fyy_y
 
 # 定义损失函数，包含PDE损失和边界条件损失
-def loss_fn(model, step, x_interior, x_boundary_fixed, y_boundary_fixed, x_boundary, y_boundary, epoch):
-    # 将边界点加入内部点中，保证边界点同样满足弹性力学本构方程
-    x_interior_loss_total = torch.cat((x_interior, x_boundary_fixed, x_boundary), dim=0)
-    x_interior_loss_total.requires_grad_(True)
-    x_interior_loss_total = x_interior_loss_total.to(device)
+def loss_fn(model, step, step_bc, inside, boundary, boundary_stress):
+    # 各边数据点个数
+    length_point = int(length / step)
+    height_point = int(height / step)
+    inside_point = (length_point - 1) * (height_point - 1)
+    length_point = int(length / step_bc)
+    height_point = int(height / step_bc)
+    left_righr_point = (height_point + 1) * 2
+    up_down_point = (length_point - 1) * 2
+
+    x_interior = torch.cat((inside, boundary), dim=0)
+    x_interior.requires_grad_(True)
+    x_interior = normalize_data(x_interior)
 
     # 计算损失
-    x_interior_loss_total = normalize_data(x_interior_loss_total)
-    pred = model(x_interior_loss_total)
+    pred = model(x_interior)
+    pred.requires_grad_(True)
+    pred = pred.to(device)
+
     u_pred, v_pred = pred[:, 0], pred[:, 1]
     sigma_xx_pred, sigma_yy_pred, sigma_xy_pred = pred[:, 2], pred[:, 3], pred[:, 4]
 
-    sigma_xx, sigma_yy, sigma_xy = calculate_sigma(u_pred, v_pred, x_interior_loss_total)
-
-    fxx_x_pred, fxy_x_pred, fxy_y_pred, fyy_y_pred = calculate_f(sigma_xx, sigma_yy, sigma_xy, x_interior_loss_total)
+    sigma_xx, sigma_yy, sigma_xy = calculate_sigma(u_pred, v_pred, x_interior)
+    fxx_x_pred, fxy_x_pred, fxy_y_pred, fyy_y_pred = calculate_f(sigma_xx_pred, sigma_yy_pred, sigma_xy_pred, x_interior)
+    # fxx_x_pred, fxy_x_pred, fxy_y_pred, fyy_y_pred = calculate_f(sigma_xx, sigma_yy, sigma_xy, x_interior)
 
     # 本构损失
     phy_loss = (torch.mean((sigma_xx_pred - sigma_xx) ** 2) +
@@ -165,29 +146,31 @@ def loss_fn(model, step, x_interior, x_boundary_fixed, y_boundary_fixed, x_bound
                 torch.mean((sigma_yy_pred - sigma_yy) ** 2))
 
     # 平衡方程损失
-    fxx_x = torch.cat((fxx_x_pred[:1000, ], fxx_x_pred[1200:1596,]), dim=0)
-    fxy_x = torch.cat((fxy_x_pred[:1000, ], fxy_x_pred[1200:1596,]), dim=0)
-    fxy_y = torch.cat((fxy_y_pred[:1000, ], fxy_y_pred[1200:1596,]), dim=0)
-    fyy_y = torch.cat((fyy_y_pred[:1000, ], fyy_y_pred[1200:1596,]), dim=0)
+    up_point = inside_point + (height_point + 1)
+    right_point = inside_point + (height_point + 1) + up_down_point
+
+    fxx_x = torch.cat((fxx_x_pred[:inside_point, ], fxx_x_pred[up_point: right_point, ]), dim=0)
+    fxy_x = torch.cat((fxy_x_pred[:inside_point, ], fxy_x_pred[up_point: right_point, ]), dim=0)
+    fxy_y = torch.cat((fxy_y_pred[:inside_point, ], fxy_y_pred[up_point: right_point, ]), dim=0)
+    fyy_y = torch.cat((fyy_y_pred[:inside_point, ], fyy_y_pred[up_point: right_point, ]), dim=0)
 
     balence_without_F_loss = (torch.mean((fxx_x + fxy_y) ** 2) + torch.mean((fxy_x + fyy_y) ** 2))
 
     # 右端载荷损失
-    pred_right = pred[1596:,]
+    pred_right = pred[right_point:,]
     sigma_xx_pred_right, sigma_yy_pred_right, sigma_xy_pred_right = pred_right[:, 2], pred_right[:, 3], pred_right[:, 4]
 
-    balence_F_loss = (torch.mean((sigma_xx_pred_right - y_boundary[:, 0]) ** 2) +
-                      torch.mean((sigma_yy_pred_right - y_boundary[:, 1]) ** 2) +
-                      torch.mean((sigma_xy_pred_right - y_boundary[:, 1]) ** 2))
+    balence_F_loss = (torch.mean((sigma_xx_pred_right - boundary_stress) ** 2) +
+                      torch.mean(sigma_yy_pred_right ** 2) +
+                      torch.mean(sigma_xy_pred_right ** 2))
 
     # 上下端应力损失
-    pred_up_down = pred[1200:1596,]
+    pred_up_down = pred[up_point: right_point,]
     sigma_xx_pred_up_down, sigma_yy_pred_up_down, sigma_xy_pred_up_down = pred_up_down[:, 2], pred_up_down[:, 3], pred_up_down[:, 4]
-
     balence_up_down_F_loss = (torch.mean((sigma_yy_pred_up_down) ** 2) + torch.mean((sigma_xy_pred_up_down) ** 2))
 
     # 固定端条件损失
-    fixed_pred = pred[1000:1200,]
+    fixed_pred = pred[inside_point: up_point,]
 
     u_pred_fixed = fixed_pred[:, 0] * beta
     v_pred_fixed = fixed_pred[:, 1] * beta
@@ -195,13 +178,14 @@ def loss_fn(model, step, x_interior, x_boundary_fixed, y_boundary_fixed, x_bound
     fixed_loss = (torch.mean((u_pred_fixed) ** 2) + torch.mean((v_pred_fixed) ** 2))
 
     # 强制性使得位移为正
-    uv_loss = (torch.sum(torch.clamp(u_pred * beta, max=0.0) ** 2) + torch.sum(torch.clamp(v_pred * beta, max=0.0) ** 2))
+    uv_loss = (torch.sum(torch.clamp(u_pred * beta, max=0.0) ** 2) + torch.sum(
+        torch.clamp(v_pred * beta, max=0.0) ** 2))
 
     lambda_balence = 1  # 平衡方程条件权重
     lambda_fixed = 1  # 固定端边界条件权重
     lambda_BC = 1  # 应力边界条件权重
     lambda_phy = 1  # 本构条件权重
-    lambda_uv = 1 # 强制横向位移为正
+    lambda_uv = 1  # 强制横向位移为正
 
     PINN_loss = (lambda_balence * balence_without_F_loss +
                  lambda_fixed * fixed_loss +
@@ -212,88 +196,122 @@ def loss_fn(model, step, x_interior, x_boundary_fixed, y_boundary_fixed, x_bound
     return (PINN_loss, balence_without_F_loss, fixed_loss, balence_F_loss + balence_up_down_F_loss, phy_loss, uv_loss)
 
 # 主训练过程
-# 主训练过程
-def train(maxiters, n, num_phi_train, step):
+def train(maxiters, step, step_bc):
     model = PINN().to(device)  # 初始化PINN模型并移动到GPU
-    optimizer = optim.Adam(model.parameters(), lr=0.0001)  # 使用Adam优化器
+    optimizer = optim.Adam(model.parameters(),lr=0.0001)  # 使用Adam优化器
     loss_history = []
 
-    steps_count = maxiters // n  # 每次加点训练的次数
-    num_print_epoch = 1000
+    # -------------------------- 设置训练集 -------------------------- #
+    x = torch.arange(step, length, step)
+    y = torch.arange(step, height, step)
 
-    # 悬臂梁尺寸
-    length = 20.0
-    height = 5.0
+    # 内部点坐标
+    inside = torch.stack(torch.meshgrid(x, y, indexing='ij')).reshape(2, -1).T
 
-    # 边界点取点数
-    x_boundary_fixed, y_boundary_fixed, x_boundary, y_boundary = (
-        generate_BC_training_data(length, height, step))
-    x_boundary_fixed.requires_grad_(True), y_boundary_fixed.requires_grad_(True)
-    x_boundary.requires_grad_(True), y_boundary.requires_grad_(True)
+    y = torch.arange(0, height + step_bc, step_bc)
+    x = torch.arange(step_bc, length, step_bc)
+    # 上侧
+    boundary_up = torch.stack(torch.meshgrid(x, y[-1], indexing='ij')).reshape(2, -1).T
+    # 下侧
+    boundary_down = torch.stack(torch.meshgrid(x, y[0], indexing='ij')).reshape(2, -1).T
 
-    # 初始化空的内部点
-    x_interior_total = torch.empty(0, 2, device=device)
+    x = torch.arange(0, length + step_bc, step_bc)
+    # 左侧
+    boundary_left = torch.stack(torch.meshgrid(x[0], y, indexing='ij')).reshape(2, -1).T
+    # 右侧
+    boundary_right = torch.stack(torch.meshgrid(x[-1], y, indexing='ij')).reshape(2, -1).T
+    # 整合
+    boundary = torch.cat([boundary_left, boundary_down, boundary_up, boundary_right])
 
-    for i in range(n):
-        train_interior = int(num_phi_train / n)
-        step_train_start_time = time.time()
+    # 右侧边界点数值
+    boundary_stress = torch.arange(height, -step_bc, -step_bc)
 
-        if i < n:
-            x_interior = torch.rand((train_interior, 2), device=device)  # 随机生成内部点
-            x_interior[:, 0] *= length
-            x_interior[:, 1] *= height
-            x_interior.requires_grad_(True)
+    # 数据处理
+    inside.requires_grad_(True)
+    inside = inside.to(device)
+    boundary.requires_grad_(True)
+    boundary = boundary.to(device)
+    boundary_stress = boundary_stress.to(device)
+    boundary_stress.requires_grad_(True)
+    # -------------------------- 数据集设置完成 -------------------------- #
 
-            x_interior_total = torch.cat((x_interior_total, x_interior), dim=0)  # 将新生成的点加入总的内部点集合
-            x_interior_total.requires_grad_(True)
+    step_start_time = time.time()
+    adams_start_time = time.time()
 
-            for epoch in range(steps_count):
-                if epoch < 10000:
+    for epoch in range(maxiters):
+        if epoch < 1000:
+            optimizer.zero_grad()
+            loss, balence_without_F_loss, fixed_loss, balence_F_loss, phy_loss, uv_loss = \
+                loss_fn(model, step, step_bc, inside, boundary, boundary_stress)
+            loss.backward(retain_graph=True)
+            nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)  # 梯度裁剪
+            optimizer.step()
+
+            loss_history.append(loss.item())
+
+            if epoch % 1000 == 0:
+                step_train_time = time.time() - step_start_time
+                print(f'Epoch {epoch}, '
+                      f'Loss: {loss.item():.6f}, '
+                      f'balence_without_F_loss: {balence_without_F_loss.item():.6f}, '
+                      f'fixed_loss: {fixed_loss.item():.6f}, '
+                      f'balence_F_loss: {balence_F_loss.item():.6f}, '
+                      f'phy_loss: {phy_loss.item():.6f}, '
+                      f'uv_loss: {uv_loss.item():.6f}，'
+                      f'Step_Time: {step_train_time:.2f}s')
+                step_start_time = time.time()
+        else:
+            if epoch == 1000:
+                optimizer.zero_grad()
+                loss, balence_without_F_loss, fixed_loss, balence_F_loss, phy_loss, uv_loss = \
+                    loss_fn(model, step, step_bc, inside, boundary, boundary_stress)
+                loss.backward(retain_graph=True)
+                nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)  # 梯度裁剪
+                optimizer.step()
+                loss_history.append(loss.item())
+
+                step_train_time = time.time() - step_start_time
+                print(f'Epoch {epoch}, '
+                      f'Loss: {loss.item():.6f}, '
+                      f'balence_without_F_loss: {balence_without_F_loss.item():.6f}, '
+                      f'fixed_loss: {fixed_loss.item():.6f}, '
+                      f'balence_F_loss: {balence_F_loss.item():.6f}, '
+                      f'phy_loss: {phy_loss.item():.6f}, '
+                      f'uv_loss: {uv_loss.item():.6f}，'
+                      f'Step_Time: {step_train_time:.2f}s')
+                step_start_time = time.time()
+
+                adams_time = time.time() - adams_start_time
+                print(f'Adams time: {adams_time:.2f}s')
+            else:
+                optimizer = torch.optim.LBFGS(model.parameters(), lr=1.0,
+                                              max_iter=5000, max_eval=5000,
+                                              history_size=50, tolerance_grad=1e-7,
+                                              tolerance_change=1.0 * np.finfo(float).eps,
+                                              line_search_fn="strong_wolfe",)
+
+                def closure():
                     optimizer.zero_grad()
                     loss, balence_without_F_loss, fixed_loss, balence_F_loss, phy_loss, uv_loss = \
-                        loss_fn(model, step, x_interior_total,
-                                x_boundary_fixed, y_boundary_fixed,
-                                x_boundary, y_boundary, epoch)
-                    loss.backward(retain_graph=True)
-                    nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)  # 梯度裁剪
-                    optimizer.step()
+                        loss_fn(model, step, step_bc, inside, boundary, boundary_stress)
+                    loss.backward()
+                    return loss
 
-                    loss_history.append(loss.item())
+                loss = optimizer.step(closure)  # LBFGS优化
 
-                    if epoch % num_print_epoch == 0:
-                        print(f'Iteration {i + 1}/{n}, Epoch {epoch}, '
-                              f'Loss: {loss.item():.6f}, '
-                              f'balence_without_F_loss: {balence_without_F_loss.item():.6f}, '
-                              f'fixed_loss: {fixed_loss.item():.6f}, '
-                              f'balence_F_loss: {balence_F_loss.item():.6f}, '
-                              f'phy_loss: {phy_loss.item():.6f}, '
-                              f'uv_loss: {uv_loss.item():.6f}')
-                else:
-                    optimizer = torch.optim.LBFGS(model.parameters(), lr=1.0,
-                                                  max_iter=50000, max_eval=50000,
-                                                  history_size=50, tolerance_grad=1e-7,
-                                                  tolerance_change=1.0 * np.finfo(float).eps,
-                                                  line_search_fn="strong_wolfe",)
+                loss_history.append(loss.item())
 
-                    def closure():
-                        optimizer.zero_grad()
-                        loss, balence_without_F_loss, fixed_loss, balence_F_loss, phy_loss, uv_loss = \
-                            loss_fn(model, step, x_interior_total,
-                                    x_boundary_fixed, y_boundary_fixed,
-                                    x_boundary, y_boundary, epoch)
-                        loss.backward()
-                        return loss
-
-                    loss = optimizer.step(closure)  # LBFGS优化
-
-                    loss_history.append(loss.item())
-
-                    if epoch % 100 == 0:
-                        print(f'Iteration {i + 1}/{n}, Epoch {epoch}, Loss: {loss.item():.5f}')
-
-
-        step_train_time = time.time() - step_train_start_time
-        print(f'Step {i + 1}/{n}, Time Elapsed: {step_train_time:.2f}s')
+                if epoch % 100 == 0:
+                    step_train_time = time.time() - step_start_time
+                    print(f'Epoch {epoch}, '
+                          f'Loss: {loss.item():.6f}, '
+                          f'balence_without_F_loss: {balence_without_F_loss.item():.6f}, '
+                          f'fixed_loss: {fixed_loss.item():.6f}, '
+                          f'balence_F_loss: {balence_F_loss.item():.6f}, '
+                          f'phy_loss: {phy_loss.item():.6f}, '
+                          f'uv_loss: {uv_loss.item():.6f}，'
+                          f'Step_Time: {step_train_time:.2f}s')
+                    step_start_time = time.time()
 
     return model, loss_history
 
@@ -396,39 +414,22 @@ def plot_results(model, loss_history):
     plt.ylabel('Height')
     plt.show()
 
-    # # 绘制平衡方程云图
-    # plt.figure()
-    # plt.imshow(fxx_x + fxy_y, extent=(0, 20, 0, 5), origin='lower', cmap='jet')
-    # plt.colorbar()
-    # plt.title('fxx_x + fxy_y')
-    # plt.xlabel('Length')
-    # plt.ylabel('Height')
-    # plt.show()
-    #
-    # plt.figure()
-    # plt.imshow(fxy_x + fyy_y, extent=(0, 20, 0, 5), origin='lower', cmap='jet')
-    # plt.colorbar()
-    # plt.title('fxy_x + fyy_y')
-    # plt.xlabel('Length')
-    # plt.ylabel('Height')
-    # plt.show()
-
     # 绘制损失函数曲线
     plt.figure()
-    plt.plot(loss_history[:])
+    plt.plot(loss_history[500:])
     plt.title('Loss Function')
     plt.xlabel('Iteration')
     plt.ylabel('Loss')
     plt.show()
 
 if __name__ == "__main__":
-    num_phi_train = 1000  # 总加点个数
-    n = 1  # 加点轮次
-    maxiters = 10200  # 总共训练的次数
-    step = 200  # 定义边界加点个数
+    maxiters = 1500  # 总共训练的次数
+    step = 0.25  # 定义内部加点
+    step_bc = 0.1  # 定义边界加点
 
     start_time = time.time()
-    model, loss_history = train(maxiters, n, num_phi_train, step)  # 传递step参数
+    model, loss_history = train(maxiters, step, step_bc)
+
     elapsed_time = time.time() - start_time
     print(f'Time Elapsed: {elapsed_time:.2f}s')
     plot_results(model, loss_history)
